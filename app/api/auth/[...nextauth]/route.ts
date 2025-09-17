31b02121176c7cfe65778584c3da3c05ab84bc7d
Shubham Mishra <sahilmishra2016@gmail.com>
2025-09-17 10:15:09 +0530

    updates

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/app/libs/auth"; // ✅ import from a separate file
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST }; // ✅ valid exports
